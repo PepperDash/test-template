46ce7f88804fb8bd5e3e86b0754e825886ba1fa6
jtalborough <jtalborough@gmail.com>
2024-10-15 14:28:46 -0400

    test bad commit

--- scripts/setup-hooks.py
+++ scripts/setup-hooks.py
@@ -17,6 +17,7 @@ def create_hooks_directory():
         print(f"Hooks directory {HOOKS_DIR} already exists.")
 
 
+
 def download_hook():
     hook_path = os.path.join(HOOKS_DIR, "pre-commit")
     print(f"Downloading the latest hooks from {HOOKS_REPO_URL}")
